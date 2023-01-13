886fba56ea5f3d266daf4902e64f1a6009c95e14
Andietz <andietz.orion@gmail.com>
2023-01-13 07:56:12 +0700

    final commit

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("rtp-senin", "root", "", {
+const sequelize = new Sequelize("sequelize_node", "root", "", {
   dialect: "mysql",
   host: "localhost",
   logging: false,
